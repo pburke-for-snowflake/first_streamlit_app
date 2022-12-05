b330ed433c60c97b82e3b0aa0f60e9e266d7a8f3
pburke-for-snowflake <119466088+pburke-for-snowflake@users.noreply.github.com>
2022-12-05 14:04:11 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healty Diner')
+streamlit.title('My Parents New Healty Dinesdfr')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
